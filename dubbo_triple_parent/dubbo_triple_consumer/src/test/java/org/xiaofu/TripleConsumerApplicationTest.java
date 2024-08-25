9e1eec80d12c4899e38dc37b9d4dd518b5012287
xiaofu <13781678921@163.com>
2024-08-25 21:12:28 +0800

    FEAT: 集群:客户端负载均衡的设置

--- dubbo_triple_parent/dubbo_triple_consumer/src/test/java/org/xiaofu/TripleConsumerApplicationTest.java
+++ dubbo_triple_parent/dubbo_triple_consumer/src/test/java/org/xiaofu/TripleConsumerApplicationTest.java
@@ -1,6 +1,8 @@
 package org.xiaofu;
 
+import org.apache.dubbo.common.constants.LoadbalanceRules;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.rpc.cluster.LoadBalance;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.xiaofu.tri.DubboGreeterTriple;
@@ -11,8 +13,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class TripleConsumerApplicationTest {
 
-    // registry 指定注册中心
-    @DubboReference(url = "xxx", protocol = "tri", registry = {"beijing"})
+    // registry 指定注册中心; LoadbalanceRules.RANDOM 随机的负载均衡算法
+    @DubboReference(url = "xxx", protocol = "tri", registry = {"beijing"}, loadbalance = LoadbalanceRules.RANDOM)
     private DubboGreeterTriple.GreeterImplBase greeterImplBase;
 
     @Test
